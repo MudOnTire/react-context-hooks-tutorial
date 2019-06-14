d9dda88fe0ba29e612008814a9d34e2d4a00b801
MudOnTire <895157882@qq.com>
2019-06-14 22:40:20 +0800

    add files

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 **Context**
 
-React的Context API提供了一种简洁的在组件间共享状态的方式，不用手动的在组件之间传递props
+React的Context API提供了一种简洁的在组件树间共享数据的方式。当项目变大，组件层级变多，通过传递props的方式共享数据就变得难以维护，context提供了一种新的数据共享的方式：将数据集中存储在一个地方，所有的组件都能从中获取数据。听起来是不是很像redux的做法？事实上，redux内部就依赖于context实现。
 
 **Hooks**
 
---
+++ src/components/BookList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class BookList extends Component {
+  render() {
+    return (
+      <div className='book-list'>
+        <ul>
+          <li>the way of kings</li>
+          <li>the name of the wind</li>
+          <li>the final empire</li>
+        </ul>
+      </div>
+    )
+  }
+}
