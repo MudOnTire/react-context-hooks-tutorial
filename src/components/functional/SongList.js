4691f3f79c050e8826d8814d734bc83c57f3ac17
MudOnTire <895157882@qq.com>
2019-06-23 11:38:41 +0800

    add use effect

--- src/components/functional/SongList.js
+++ src/components/functional/SongList.js
@@ -1,20 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import uuid from 'uuid/v1';
+import NewSongForm from './NewSongForm';
 
 const SongList = () => {
 
   const [songs, setSongs] = useState([
-    // { title: 'almost home', id: 1 },
-    // { title: 'memory gospel', id: 2 },
-    // { title: 'this wild darkness', id: 3 }
+    { title: 'almost home', id: 1 },
+    { title: 'memory gospel', id: 2 },
+    { title: 'this wild darkness', id: 3 }
   ]);
+  const [age, setAge] = useState(20);
+  const addSong = (title) => {
+    setSongs([
+      ...songs,
+      { title, id: uuid() }
+    ])
+  }
+
+  useEffect(() => {
+    console.log('songs change');
+  }, [songs]);
+
+  
+  useEffect(() => {
+    console.log('age changes');
+  }, [age]);
 
   return (
     <div className='song-list'>
-      {songs[1]?.title}
       <ul>
-        <li>this wild darkness</li>
-        <li>memory gospel</li>
+        {
+          songs.map(song => (
+            <li key={song.id}>{song.title}</li>
+          ))
+        }
       </ul>
+      <NewSongForm addSong={addSong} />
+      <button onClick={() => { setAge(age + 1) }}>Add 1 to age: {age}</button>
     </div>
   )
 }
